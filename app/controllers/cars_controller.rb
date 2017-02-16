e2a3a770fbbc7247304bd244b487b771fd0c6c84
jrkinouani <kinouani@gmail.com>
2017-02-16 12:02:26 +0400

    hotfix

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,7 +2,7 @@ class CarsController < ApplicationController
   before_action :find_car, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @cars = Car.all.order("created_at DESC")
   end
 
   def show
@@ -22,10 +22,27 @@ class CarsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @car.update(car_params)
+      redirect_to @car
+    else
+      render  'edit'
+    end
+  end
+
+  def destroy
+    @car.destroy
+    redirect_to root_path
+  end
+
   private
 
   def car_params
-    params.require(:car).permit(:title, :description,:price_seven_days)
+    params.require(:car).permit(:title, :description,:price_seven_days,:price_ten_days, :price_fifteen_days,
+    :price_fifteenmore_days, :price_twenty_days, :price_month_days)
   end
 
   def find_car
